7c7f36e5e45d4831e6fd68f81c491561d5808aee
Loazo <35292177+Loazo@users.noreply.github.com>
2018-01-24 09:59:29 +0100

    Update LevelCreator.cpp

--- Ancien morceau de code/fonction/LevelCreator.cpp
+++ Ancien morceau de code/fonction/LevelCreator.cpp
@@ -1,3 +1,23 @@
+void DisplayGridCreator(unsigned & i, vector <string> & StockDeVar, unsigned & GridSize)
+{
+    ClearScreen();
+    cout << "Entrez la ligne " << i+1 << " sur "<< GridSize <<" chiffres avec un 0 pour les cases ordinaires et" << endl << "un 1 pour les murs (ils peuvent tomber)" << endl << endl;
+    if(i != 0)
+    {
+        for(unsigned j = 0; j < i ; ++j)
+        {
+            for(unsigned k = 0; k < StockDeVar.size() ; ++k)
+            {
+                cout << StockDeVar[4+j][k];
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+}
+
+
+
 bool TestValeurDansString(string & Str, unsigned & GridSize)
 {
     if(Str.size() != GridSize) return false;
@@ -12,89 +32,113 @@ bool TestValeurDansString(string & Str, unsigned & GridSize)
 }// TestValeurDansString ()
 
 void LevelCreator() {
-    cout << "Choisissez un nom pour votre niveau (max 20 caracteres et ne pas utiliser deux fois le meme nom ou ne rien saisir)" << endl;
+    cout << "Choisissez un nom pour votre niveau (max 20 caracteres et ne pas utiliser deux fois le meme nom ou ne rien saisir)" << endl << endl;
     string NomLevel; // rajouter le test d'existence du fichier
     while(true) {
         bool NoSlash=true;
         NomLevel="reset";
         getline(cin,NomLevel);
         if(NomLevel.size() > 20 || NomLevel.size() == 0) {
-            cout << "Respectez les consignes !!!" << endl;
+            ClearScreen();
+            cout << "Choisissez un nom pour votre niveau (max 20 caracteres et ne pas utiliser deux fois le meme nom ou ne rien saisir)" << endl << endl;
+            cout << "Respectez les consignes !!!" << endl << endl;
             continue;
         }
         for(unsigned i=0; i < NomLevel.size(); ++i) {
             if(NomLevel[i]=='/') {
-                cout << "Les / sont interdits dans les noms de fichier sous linux" << endl;
+                ClearScreen();
+                cout << "Choisissez un nom pour votre niveau (max 20 caracteres et ne pas utiliser deux fois le meme nom ou ne rien saisir)" << endl << endl;
+                cout << "Les / sont interdits dans les noms de fichier sous linux" << endl << endl;
                 NoSlash=false;
                 break;
             }
         }
         if(NoSlash)break;
     }
-    cout << "Entrez la taille de la grille" << endl;
+    ClearScreen();
+    cout << "Entrez la taille de la grille" << endl << endl;
     unsigned GridSize;
     while(true) {
         cin >> GridSize;
         if(cin.fail() || GridSize >= 20) {
             ClearBuf();
-            cout << "Entrez un nombre positif strictement inferieur a 20 (faut pas abuser quand meme)" << endl;
+            ClearScreen();
+            cout << "Entrez la taille de la grille" << endl << endl;
+            cout << "Entrez un nombre positif strictement inferieur a 20 (faut pas abuser quand meme)" << endl << endl;
             continue;
         }
         break;
     }
-    cout << "Entrez le score a atteindre pour reussir le niveau" << endl;
+    ClearScreen();
+    cout << "Entrez le score a atteindre pour reussir le niveau (max 9998)" << endl << endl;
     unsigned ScoreToWin;
     while(true) {
         cin >> ScoreToWin;
-        if(cin.fail()) {
+        if(cin.fail() || ScoreToWin > 9998) {
             ClearBuf();
-            cout << "Entrez un nombre positif" << endl;
+            ClearScreen();
+            cout << "Entrez le score a atteindre pour reussir le niveau (max 9998)" << endl << endl;
+            cout << "Entrez un nombre positif inferieur a 9998" << endl << endl;
             continue;
         }
         break;
     }
-    cout << "Entrez le nombre de valeurs que les cases de la grille peuvent adopter" << endl;
+    ClearScreen();
+    cout << "Entrez le nombre de valeurs que les cases de la grille peuvent adopter" << endl << endl;
     unsigned NbKandies;
     while(true) {
         cin >> NbKandies;
-        if(cin.fail() || NbKandies > 7) {
+        if(cin.fail() || NbKandies > 7 || NbKandies == 0) {
             ClearBuf();
-            cout << "Entrez un nombre positif strictement inferieur à 8 car le terminal ne nous donne acces qu'a 8 couleurs et y'a des murs ! ;{" << endl;
+            ClearScreen();
+            cout << "Entrez le nombre de valeurs que les cases de la grille peuvent adopter" << endl << endl;
+            cout << "Entrez un nombre strictement positif et strictement inferieur à 8 car le terminal ne nous donne acces qu'a 8 couleurs et qu'il y a des murs ! ;{" << endl << endl;
             continue;
         }
         break;
     }
-    cout << "Entrez le nombre de coups possibles" << endl;
+    ClearScreen();
+    cout << "Entrez le nombre de coups possibles" << endl << endl;
     unsigned Coups;
     while(true) {
         cin >> Coups;
         if(cin.fail()) {
             ClearBuf();
-            cout << "Entrez un nombre positif !" << endl;
+            ClearScreen();
+            cout << "Entrez le nombre de coups possibles" << endl << endl;
+            cout << "Entrez un nombre positif !" << endl << endl;
             continue;
         }
         break;
     }
-    cout << "Entrez les " << GridSize << " lignes de " << GridSize << " chiffres avec un 0 pour les cases ordinaires et" << endl << "un 1 pour les murs (ils peuvent tomber)" << endl;
     vector <string> StockDeVar;
     StockDeVar.resize(GridSize+4);
     StockDeVar[0]=to_string(GridSize);
     StockDeVar[1]=to_string(NbKandies);
     StockDeVar[2]=to_string(Coups);
     StockDeVar[3]=to_string(ScoreToWin);
-    //ofs << GridSize << endl << NbKandies << endl << Coups << endl << ScoreToWin << endl;
+    string LigneTmp;
+    ClearBuf();
     for(unsigned i=0; i < GridSize; ++i) {
-        string LigneTmp;
-        getline(cin,LigneTmp);
-        while(!TestValeurDansString(LigneTmp, GridSize)) {
-            cout << "Veuillez respecter les indications données" << endl;
-            getline(cin,LigneTmp);
+
+        DisplayGridCreator(i,StockDeVar,GridSize);
+
+        while(true) {
+            getline(cin, LigneTmp);
+            if(!TestValeurDansString(LigneTmp, GridSize)) {
+                ClearScreen();
+                DisplayGridCreator(i,StockDeVar,GridSize);
+                cout << "Veuillez respecter les indications données !" << endl << endl;
+                continue;
+            }
+            ClearScreen();
+            break;
         }
-        StockDeVar[i+4]=LigneTmp;
+    StockDeVar[i+4]=LigneTmp;
     }
     ofstream ofs(NomLevel);
     for(unsigned i=0; i < StockDeVar.size(); ++i) {
         ofs << StockDeVar[i] << endl;
     }
-    cout << endl << "Bravo, votre niveau a ete cree avec succes" << endl;
+    cout << "Bravo, votre niveau a ete cree avec succes" << endl;
 } // LevelCreator ()
