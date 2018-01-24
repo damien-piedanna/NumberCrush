c54d7436f3e37a1a6013e8e34129f294c8b691d0
SkyDuriel <35491689+SkyDuriel@users.noreply.github.com>
2018-01-24 10:02:07 +0100

    Update Todo.cpp

--- Todo.cpp
+++ Todo.cpp
@@ -1,2 +1,8 @@
 -Rajouter dans DisplayFile tout les alternatives (quand le programme n'arrive pas à charger le fichier)
+                                                 
+-Rajouter la fonction de clément pour créer les niveaux   NumberCrushIUT/Ancien morceau de code/fonction/LevelCreator.cpp
+        La fonction marche normalement. Juste en executant LevelCreator.cpp on arrive à créer un niveau.
+                                                  
+-Corriger Bug trouver dans le compte rendue du test
 
+Optionnel : Rajouter le power-up
