7c0feb8af2404dc0b3e70d921489880e2835386e
SkyDuriel <35491689+SkyDuriel@users.noreply.github.com>
2018-01-24 08:40:13 +0100

    Update Todo.cpp

--- Todo.cpp
+++ Todo.cpp
@@ -1,9 +1,2 @@
--Modifier la Fonction d'affichage du score. Rajouter qu'on puisse mettre en argument le noms du fichier.
-
 -Rajouter dans DisplayFile tout les alternatives (quand le programme n'arrive pas à charger le fichier)
 
--Segmenter
-
--Création de niveau
-
--Fichier Paramètre
